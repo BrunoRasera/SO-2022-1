a9f350c27a1dcdbdc8ee6703fbc550d66d551873
Daniel Paiva <danielpaiva1964@gmail.com>
2022-07-21 17:13:59 -0300

    Detalhes sobre o campo deslocamento

--- T4-Paginacao/exemploStructKimura.c
+++ T4-Paginacao/exemploStructKimura.c
@@ -3,13 +3,18 @@
 #define SMR 2
 #define SSW 3
 
+//MV > MR e SW = MV - MR
+
 struct pagina_t
 {
-    uint16_t presente:1;
+    uint16_t presente:1;//bitsignal->usando apenas 1bit dos 16alocados pelo uint16_t
     uint16_t referenciada:1;
     uint16_t modificada:1;
-    uint16_t pad:1;
-    uint16_t deslocamento:12;
+    uint16_t pad:1;//Pra fechar os 16bits
+    uint16_t deslocamento:12;//12bits da 4kb e o primeiro pagina t seria de 0 a 4kb na memoria real, ele é o mapeamento da mem vitual pra real
+    //se smv é 2 MV[0]->Ok a 4k MV[1]=4k a 8k, o deslocamento do primeiro seria a representação de 0kb a 4k
+    //Ele é só uma representação... Se a CPU quer acessar 5300, vc só teria que ver onde que isso da na memoria virtual, o primeiro é de 0 a 4 e o outro de 5 a 8, então vou no segundo
+    
 };
 
 struct pagina_t mv[SMV], mr[SMR], sw[SSW];
@@ -27,4 +32,9 @@ struct pagina_t mv[SMV], mr[SMR], sw[SSW];
  a pagina, verifica-se se ela está presente ou ausente. Se acessou a página e a página está presente, então não fez nada, só atualiza a contagem de
  conferências feitas mas nn mexe na de pagemiss. (LEMBRAR DE ATUALIZAR O BIT REFERENCIADA!!)
  - Posso gerar eventos de memoria onde ocorrem mudanças na página (alterar o bit modificada)
+ 
+ 
+ 
+ 
+ 
  */
